3e901b05925a32f5e88c0bfc28934ece2eac4a12
Md Irshad <32608381+irshadmd@users.noreply.github.com>
2021-07-25 15:27:32 +0530

    tree implementation completed

--- commands/tree.js
+++ commands/tree.js
@@ -1,8 +1,26 @@
 //tree file code
+let fs = require("fs");
+let path = require("path");
 
 function tree(path) {
     console.log(`tree command executed with path "${path}"`);
+    let currentPath = path;
 
+    findContent(currentPath);
+}
+
+function findContent(currentPath) {
+
+    let allContents = fs.readdirSync(currentPath);
+    for (let i = 0; i < allContents.length; i++) {
+        let p = path.join(currentPath, allContents[i]);
+        if (fs.lstatSync(p).isFile()) {
+            console.log("\t|---- " + allContents[i]);
+        } else {
+            console.log("|--------------- " + allContents[i] + " ---------------|");
+            findContent(p)
+        }
+    }
 }
 
 module.exports = {
